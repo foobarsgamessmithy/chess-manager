543c57d2eed56012e52f56041ec2d2311b0cb40f
ALEXONTHEROAD\alexb <xyz.foobar42@gmail.com>
2024-10-16 22:42:04 +0200

    init

---
+++ src/main/java/eu/foobarsgamesmithy/chessmanager/ChessmanagerApplication.java
@@ -0,0 +1,13 @@
+package eu.foobarsgamesmithy.chessmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/foobarsgamesmithy/chessmanager/ChessmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.foobarsgamesmithy.chessmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
